cfce41e3ca62a3e8346fd35b42f872af9bd2c9d3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-08-09 14:06:38 +0100

    task 5 begins

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,14 @@
 import uuid
 import datetime
+from models import storage
 '''
 defines the class baseModel for our instances
 '''
 
+
 class BaseModel:
     '''class BaseModel'''
+
     def __init__(self, *args, **kwargs):
         if kwargs is None:
             for key, value in kwargs.items():
@@ -19,19 +22,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.save()
+            storage.new(self)
 
-
-
-    
     def __str__(self):
         # should print in format [<class name>] (<self.id>) <self.__dict__>
         return "{} ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
         self.updated_at = datetime.datetime.now()
+        storage.save()
+
     def to_dict(self):
         dicts = self.__dict__
         dicts['__class__'] = (self.__class__.__name__)
         dicts['created_at'] = self.created_at.isoformat()
         dicts['updated_at'] = self.updated_at.isoformat()
         return (dicts)
-    
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,26 @@
 import json
 
+
 class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return (self.__objects)
+
+    def new(self, obj):
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        # serializer
+        for k, v in self.__objects.items():
+            with open(self.__file_path, 'w') as f:
+                f.write(json.dumps(self.__objects[k]))
+
+    def reload(self):
+        # deserializer
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.loads(f.read())
+        except:
+            pass
