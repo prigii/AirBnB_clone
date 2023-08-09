29310f255d19a9a0768e857d8e7fcafb9b26083b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-08-09 15:00:32 +0100

    should work now, updated some funcitions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,21 +6,25 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return (self.__objects)
+        """returns dictionary __objects"""
+        return (FileStorage.__objects)
 
     def new(self, obj):
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        FileStorage.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         # serializer
         for k, v in self.__objects.items():
-            with open(self.__file_path, 'w') as f:
-                f.write(json.dumps(self.__objects[k]))
+            with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+                d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+                json.dump(d, f)
 
     def reload(self):
         # deserializer
         try:
-            with open(self.__file_path, 'r') as f:
-                self.__objects = json.loads(f.read())
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                FileStorage.__objects = json.loads(f.read())
         except:
-            pass
+            return
