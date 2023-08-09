76abf1fb96df31b76b3b8f809370ea56f9dda267
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-08-09 15:17:31 +0100

    modified and refactored code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,24 +7,24 @@ class FileStorage:
 
     def all(self):
         """returns dictionary __objects"""
-        return (FileStorage.__objects)
+        return (self.__objects)
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        FileStorage.__objects["{}.{}".format(
+        self.__objects["{}.{}".format(
             obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         # serializer
         for k, v in self.__objects.items():
-            with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-                d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            with open(self.__file_path, 'w', encoding='utf-8') as f:
+                d = {k: v.to_dict() for k, v in self.__objects.items()}
                 json.dump(d, f)
 
     def reload(self):
         # deserializer
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as f:
-                FileStorage.__objects = json.loads(f.read())
+                self.__objects = json.loads(f.read())
         except:
             return
